0994140e464adb9756c39485ac68bd42d472ada0
amey.deshmukh <amey.deshmukh@easebuzz.in>
2018-03-28 17:35:27 +0530

    update

---
+++ paylib/src/main/java/pay/com/paylib/PayOnline.java
@@ -0,0 +1,18 @@
+package pay.com.paylib;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin-rahul on 28/3/18.
+ */
+
+public class PayOnline extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pay_online_layout);
+    }
+}
